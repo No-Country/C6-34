45f23f0df80e5e6c3d60b579d8f536b80ce8d61c
Jose Sandino <josesandinonio@gmail.com>
2022-08-11 07:40:09 -0600

    Add links in UsuarioTipo

--- encontrados/src/components/Usuariotipo/Usuariotipo.js
+++ encontrados/src/components/Usuariotipo/Usuariotipo.js
@@ -1,14 +1,19 @@
 import './Usuariotipo.css';
-import { Box, Button} from '@chakra-ui/react'
+import { Flex, Box, Button} from '@chakra-ui/react'
+import { Link } from "react-router-dom";
 const Usuariotipo = () =>{
     
     return(
-        <Box>
-            <h1 className='tittle'>Encontrados</h1>
-            <p className='select'>Selecciona tu tipo de usuario</p>
-            <Button className='btn-sup' bg='brand.secondary'>EMPRESA</Button>
-            <Button className='btn-inf' bg='brand.secondary'>USUARIO</Button>
-        </Box>
+            <Box>
+                <h1 className='tittle'>Encontrados</h1>
+                <p className='select'>Selecciona tu tipo de usuario</p>
+                <Link to="/register">
+                    <Button className='btn-sup' bg='brand.secondary'>EMPRESA</Button>
+                </Link>
+                <Link to="/register">
+                    <Button className='btn-inf' bg='brand.secondary'>USUARIO</Button>
+                </Link>
+            </Box>
     )
 }
 export default Usuariotipo;
