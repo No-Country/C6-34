6e62a83ff427e5b8d20e677baf5e868949466d98
Juan Carlos Macias <maciasnajeraj@gmail.com>
2022-08-11 10:51:01 -0500

    login page view was added

--- encontrados/src/App.js
+++ encontrados/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+//styles
 import './App.css';
-
+//tools
+import {  Routes, Route, } from "react-router-dom";
+//components
+import {Login, ChooseUser} from './components/login/login.jsx';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login/>}>
+        <Route path="/ChooseUser" element={<ChooseUser/>}/>
+      </Route>
+    </Routes>
   );
 }
 
