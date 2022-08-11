92a321e449d1fb4f614049196b8fc0da8742080f
Juan Carlos Macias <maciasnajeraj@gmail.com>
2022-08-11 11:28:53 -0500

    ChooseUser component was remove and max with for the inputs login was added

--- encontrados/src/App.js
+++ encontrados/src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 //tools
 import {  Routes, Route, } from "react-router-dom";
 //components
-import {Login, ChooseUser} from './components/login/login.jsx';
+import {Login,} from './components/login/login.jsx';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login/>}>
-        <Route path="/ChooseUser" element={<ChooseUser/>}/>
       </Route>
     </Routes>
   );
