5179d0e13fdcbe9e4bbc9b9926448a054cfa51c2
Leyly <nohely.ramosderosas@gmail.com>
2022-08-17 11:59:48 -0500

    home

--- encontrados/src/App.js
+++ encontrados/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import { Register } from './components/Register/Register';
 import Usuariotipo from './components/Usuario/Usuariotipo';
+import Home from './components/Home/Home'; 
 
 function App() {
   return (
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="register" element={<Register/>} />
           <Route path="usuario" element={<Usuariotipo/>} />
         </Routes>
