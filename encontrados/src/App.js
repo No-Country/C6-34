ed0536ac839f9ca54f2b498e2ebb5f0a47ec816f
Leyly <nohely.ramosderosas@gmail.com>
2022-09-01 15:42:57 -0300

    comments

--- encontrados/src/App.js
+++ encontrados/src/App.js
@@ -15,7 +15,6 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
           <Route path="login" element={<Login/>} />
           <Route path="register" element={<Register/>} />
           <Route path="user" element={<Usertype/>} />      
